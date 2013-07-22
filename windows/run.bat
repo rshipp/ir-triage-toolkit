aadcf2a3ca1f3e1ffb61774f8ab24c91d5a0a0e7
george <rpubaddr0@gmail.com>
2013-07-22 16:10:11 -0600

    Starting the Windows IR batch script.

--- windows/run.bat
+++ windows/run.bat
@@ -2,3 +2,12 @@
 rem Collect data.
 
 rem Make sure we're Admin.
+net file >nul 2>nul
+if errorlevel 1 (
+        echo Must be Admin!
+        pause >nul
+        goto :eof
+)
+
+rem Where should we store the data?
+
