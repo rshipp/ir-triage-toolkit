51bebc59d357cbc69226a2dfc1a16894621a2ff9
george <rpubaddr0@gmail.com>
2013-07-24 11:47:31 -0600

    Finished Windows IR script, sha512->sha256, added some more Windows binaries.

--- windows/sh/run.sh
+++ windows/sh/run.sh
@@ -20,10 +20,8 @@ saveto="$savedir/$(hostname)-$(date +%Y.%m.%d-%H.%M.%S)"
 mkdir "$saveto"
 logfile="$saveto/log.txt"
 
-
 log() {
-  echo -n "$(date +"%b %d %H:%M:%S") $(hostname) irscript: $1" | tee -a 
-"$logfile"
+  echo -n "$(date +"%b %d %H:%M:%S") $(hostname) irscript: $1" | tee -a "$logfile"
 }
 
 # Start the log.
@@ -32,13 +30,11 @@ log "# Incident response volatile data collection script."
 log "# Starting data collection..."
 
 # 1. Acquire a full memory dump.
-log "# Starting dc3dd to dump system memory..."
-memfile="$saveto/memdump-$(hostname)-linux-$(uname -m)"
-log "$bin/dc3dd if=/dev/mem hash=sha512 of='${memfile}.dd' log='${memfile}.log' 
-hlog='${memfile}.sum'"
-"$bin/dc3dd" if=/dev/mem hash=sha512 of="${memfile}.dd" \
-             log="${memfile}.log" hlog="${memfile}.sum"
-log "# dc3dd finished."
+log "# Starting Mandiant Memoryze to dump system memory..."
+memfile="$saveto/memdump-$(hostname)-windows.dd"
+log "$bin/memoryze/MemoryDD.bat --output '$memfile'"
+"$bin/memoryze/MemoryDD.bat" --output "$memfile" 
+log "# Memoryze finished."
 
 # 2. Collect network information.
 log "# Collecting network information..."
@@ -49,20 +45,24 @@ netstat -esr >> "$saveto/network.txt" 2>&1
              
 # 3. Collect information about opened files and running processes.
 log "# Collecting information about opened files and running processes."
-
+log "tasklist -V > '$saveto/tasklist.txt' 2>&1"
+tasklist -V > "$saveto/tasklist.txt" 2>&1
 
 # 4. Collect user/system information.
 log "# Collecting user/system information."
-
+log "whoami /all > '$saveto/users_whoami.txt' 2>&1"
+whoami /all > "$saveto/users_whoami.txt" 2>&1
 
 # 5. Collect device information.
-
+log "# Collecting information about currently mounted devices."
+log "diskpart /s '$bin/list.diskpart' > '$saveto/mounted_devices.txt' 2>&1"
+diskpart /s "$bin/list.diskpart" > "$saveto/mounted_devices.txt" 2>&1
 
 # Create checksums for all files
-log "# Creating checksums (sha512sum) for all files."
-log "sha512sum $saveto/* > $saveto/sha512sums.txt"
-sha512sum "$saveto/"* > "$saveto/sha512sums.txt"
-log "sed 's/^.*sha512sums.txt.*$//; s/^.*log.txt.*$//' $saveto/sha512sums.txt"
-sed 's/^.*sha512sums.txt.*$//; s/^.*log.txt.*$//' "$saveto/sha512sums.txt"
+log "# Creating checksums (sha256sum) for all files."
+log "sha256deep $saveto/* > $saveto/sha256sums.txt"
+sha256deep "$saveto/"* > "$saveto/sha256sums.txt"
+log "$bin/sed 's/^.*sha256sums.txt.*$//; s/^.*log.txt.*$//' $saveto/sha256sums.txt"
+sed 's/^.*sha256sums.txt.*$//; s/^.*log.txt.*$//' "$saveto/sha256sums.txt"
 
 log "# All tasks completed. Exiting."
